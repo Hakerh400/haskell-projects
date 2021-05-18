ba00e22f00e0d71d86d9336c419f236c7fe22854
Thomas <hakerh403@gmail.com>
2021-05-18 20:31:58 +0200

    commit

--- rank-n-types/main.hs
+++ rank-n-types/main.hs
@@ -1,12 +1,17 @@
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 main :: IO ()
 main = do
-  print $ func (:[]) head 3 "abc"
+  print $ func (:[]) head () 3
   return ()
 
-func :: (forall a. a -> [a]) -> (forall a. [a] -> a) -> Integer -> a -> a
-func f g 0 a = a
-func f g 1 a = g (f a)
-func f g 2 a = g (g (f (f a)))
-func f g 3 a = g (g (g (f (f (f a)))))
+func :: (forall b. b -> m b) -> (forall c. m c -> c) -> a -> Integer -> a
+func f g z n = func' f g z id id n
+
+func' :: (forall b. b -> m b) -> (forall c. m c -> c) -> a -> (a -> d) -> (d -> a) -> Integer -> a
+func' f g z p q 0 = q (p z)
+func' f g z p q n = func' f g z (f . p) (q . g) (n - 1)
