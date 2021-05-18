03e5b64820cb38ab34aa8f8d7d1dfea95dc1e9ca
Thomas <hakerh403@gmail.com>
2021-05-18 23:32:45 +0200

    commit

--- replicate-m/Main.hs
+++ replicate-m/Main.hs
@@ -26,13 +26,51 @@ printBlock' n i = if i <= n
 func :: [a] -> Integer -> [[a]]
 func list n = nest (list >>=) n
 
-t = (.)
+t a b c = a (b c)
 p = flip
+
+h = t
 
 -- a (b c) ---> t a b c
 
 nest :: (Monad m) => ((a -> m [a]) -> m [a]) -> Integer -> m [a]
-nest f 1 = f (t                                     return                                                             (p (:) []))
-nest f 2 = f (t f (t                             (t return)                                               (p (t t (:)) (p (:) []))))
-nest f 3 = f (t f (t (t f)                 (t (t (t return))                           (p (t t (t t (:))) (p (t t (:)) (p (:) []))))))
-nest f 4 = f (t f (t (t f) (t (t (t f)) (t (t (t (t return))) (p (t t (t t (t t (:)))) (p (t t (t t (:))) (p (t t (:)) (p (:) [])))))))) 
+nest f n = nest' f f f (t return) (p (:) []) (:) n
+
+-- nest' :: (Monad m) =>
+--   ((a -> m [a]) -> m [a]) ->
+--
+--   -- x
+--   (b -> m [a]) ->
+--
+--   -- x'
+--   () ->
+--
+--   -- y
+--   (c -> b) ->
+--
+--   -- z
+--   (c) ->
+--
+--   -- z'
+--   () ->
+--
+--   Integer ->
+--   m [a]
+nest' f x x' y z z' n = case n of
+  1 -> x (y z)
+  n -> let
+    x'' = t x'
+    z'' = t t z'
+    in nest' f
+      {- x  -} (h x x'')
+      {- x' -} x''
+      {- y  -} (t y)
+      {- z  -} (p z'' z)
+      {- z' -} z''
+      (n - 1)
+
+test :: (Monad m) => ((a -> m [a]) -> m [a]) -> Integer -> m [a]
+test f 1 = h f                                                      (t return)                                                               (p (:) [])
+test f 2 = h (h f (t f))                                         (t (t return))                                                 (p (t t (:)) (p (:) []))
+test f 3 = h (h (h f (t f)) (t (t f)))                        (t (t (t return)))                             (p (t t (t t (:))) (p (t t (:)) (p (:) [])))
+test f 4 = h (h (h (h f (t f)) (t (t f))) (t (t (t f))))   (t (t (t (t return))))   (p (t t (t t (t t (:)))) (p (t t (t t (:))) (p (t t (:)) (p (:) []))))
