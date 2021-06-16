0d403780a49569469aa9f83a41dc3109c8804c59
Thomas <hakerh403@gmail.com>
2021-06-16 15:16:06 +0200

    commit

--- factorial/Main.hs
+++ factorial/Main.hs
@@ -3,4 +3,4 @@ main = print $ fac 5
 
 fac :: Int -> Int
 fac = length . f . flip replicate (.) where
-  f a = last $ (a >> f (tail a)) : [[id] | null a]
+  f a = last $ [id] : map (const $ a >> f (tail a)) a
