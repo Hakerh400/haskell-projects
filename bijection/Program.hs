11e2fb2265002b60e6fdc86a2eb4d52ed61d42d3
Hakerh400 <hakerh403@gmail.com>
2021-09-21 05:31:10 +0200

    commit

--- bijection/Program.hs
+++ bijection/Program.hs
@@ -1,9 +1,30 @@
 module Program where
 
+import Control.Monad
 import Prelude hiding (read)
 import qualified Serializer as S
 
 type N = Integer
+type Prog = N -> N
+
+class Data t where
+  from_hs :: t -> N
+  to_hs :: N -> t
+
+instance (Data t1, Data t2) => Data (t1, t2) where
+  from_hs (a, b) = pair (from_hs a) (from_hs b)
+  to_hs = pair_get $ \a b -> (to_hs a, to_hs b)
+
+instance Data Integer where
+  from_hs = id
+  to_hs = id
+
+instance (Data t) => Data [t] where
+  from_hs = foldr (cons . from_hs) nil
+  to_hs = list_fold [] $ (:) . to_hs
+
+run :: (Data t1, Data t2) => Prog -> t1 -> t2
+run prog = to_hs . prog . from_hs
 
 data Unit = Unit N
 
@@ -73,6 +94,10 @@ bool_get f t b = case to_bool b of
 bool_fold :: (N -> a) -> (N -> a) -> N -> a
 bool_fold = bool_get
 
+mu :: (N -> N) -> N
+mu f = mu' f 0 where
+  mu' f n = nat_get (mu' f $ suc n) id (f n)
+
 data Nat =
   Zero |
   Suc Nat
@@ -100,6 +125,45 @@ nat_fold :: a -> (a -> a) -> N -> a
 nat_fold z f n = nat_fold' z f $ to_nat n where
   nat_fold' z _  Zero   = z
   nat_fold' z f (Suc n) = f $ nat_fold' z f n
+
+data List =
+  Nil |
+  Cons N List
+
+from_list :: List -> N
+from_list = S.ser . from_list' where
+  from_list' Nil = return ()
+  from_list' (Cons x xs) = do
+    S.inc1
+    S.write_nat x
+    from_list' xs
+
+to_list :: N -> List
+to_list = S.deser to_list' where
+  to_list' = do
+    b <- S.nz
+    if b
+      then do
+        x <- S.read_nat
+        xs <- to_list'
+        return $ Cons x xs
+      else return Nil
+
+nil :: N
+nil = from_list Nil
+
+cons :: N -> N -> N
+cons x xs = from_list $ Cons x $ to_list xs
+
+list_get :: a -> (N -> N -> a) -> N -> a
+list_get z f n = case to_list n of
+  Nil -> z
+  Cons x xs -> f x $ from_list xs
+
+list_fold :: a -> (N -> a -> a) -> N -> a
+list_fold z f n = case to_list n of
+  Nil -> z
+  Cons x xs -> f x $ list_fold z f $ from_list xs
 
 u :: a
 u = undefined
