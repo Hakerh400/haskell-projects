5a87ab31e58a3d2b4ae87ed9b98973a2d23370eb
Hakerh400 <hakerh403@gmail.com>
2021-10-05 20:02:04 +0200

    commit

--- theorem-prover/Logic.hs
+++ theorem-prover/Logic.hs
@@ -15,35 +15,31 @@ type Comb :: Type
 data Comb where
   K    :: Comb
   S    :: Comb
-  I    :: Comb
-  D    :: Comb
-  F    :: Comb
-  Zero :: Comb
-  Suc  :: Comb
   Call :: !Comb -> !Comb -> Comb
 
 infixl 9 .
 type (.) = Call
 
+type I    = S . K . K
+type D    = S . (K . S) . K
+type F    = S . (D . D . S) . (K . K)
+type Zero = K . (I)
+type Suc  = S . D
+
 infix 4 ==
 type (==) = Eq
 type Eq :: Comb -> Comb -> Type
 data Eq a b where
-  Refl     :: forall a. a == a
-  Com      :: forall a b. !(a == b) -> b == a
-  Tran     :: forall a b c. !(a == b) -> !(b == c) -> a == c
-  Sub      :: forall a b f. !(a == b) -> f . a == f . b
-  Ext      :: forall f g. !(forall a. f . a == g . a) -> f == g
-  KDef     :: forall a b. (K . a) . b == a
-  SDef     :: forall a b c. ((S . a) . b) . c == a . c . (b . c)
-  IDef     :: I == S . K . K
-  DDef     :: D == S . (K . S) . K
-  FDef     :: F == S . (D . D . S) . (K . K)
-  ZeroDef  :: Zero == K . (I)
-  SucDef   :: Suc == S . D
-  Induct   :: !(f Zero == r)
-           -> !(forall m. Nat m -> f m == r -> f (Suc . m) == r)
-           -> !(Nat n) -> f n == r
+  Refl    :: forall a. a == a
+  Com     :: forall a b. !(a == b) -> b == a
+  Tran    :: forall a b c. !(a == b) -> !(b == c) -> a == c
+  Cong    :: forall f g a b. !(f == g) -> !(a == b) -> f . a == g . b
+  Ext     :: forall f g. !(forall a. f . a == g . a) -> f == g
+  KDef    :: forall a b. (K . a) . b == a
+  SDef    :: forall a b c. ((S . a) . b) . c == a . c . (b . c)
+  Induct  :: !(f Zero == r)
+          -> !(forall m. Nat m -> f m == r -> f (Suc . m) == r)
+          -> !(Nat n) -> f n == r
 
 type Nat :: Comb -> Type
 data Nat a where
--- theorem-prover/Main.hs
+++ theorem-prover/Main.hs
@@ -14,17 +14,39 @@ import Logic
 main :: P.IO ()
 main = P.putStrLn "ok"
 
-tr1 :: forall x y a. x == a -> x == y -> y == a
-tr1 xa xy = Tran (Com xy) xa
+tr1 :: forall a b c. a == b -> a == c -> c == b
+tr1 ab ac = Tran (Com ac) ab
 
-tr2 :: forall x y a. a == x -> x == y -> a == y
-tr2 ax xy = Tran ax xy
+tr2 :: forall a b c. a == b -> b == c -> a == c
+tr2 = Tran
 
-skk_a_eq_a :: forall a. S . K . K . a == a
-skk_a_eq_a = SDef `tr2` KDef
+acong :: forall f a b. a == b -> f . a == f . b
+acong = Cong Refl
 
--- id :: forall a. (I).a == a
--- id = u
+fcong :: forall f g a. f == g -> f . a == g . a
+fcong fg = Cong fg Refl
+
+fcong2 :: forall f g a b. f == g -> f . a . b == g . a . b
+fcong2 fg = fcong (fcong fg)
+
+id :: forall a. I . a == a
+id = Refl `tr2` SDef `tr2` KDef
+
+dot :: forall a b c. D . a . b . c == a . (b . c)
+dot = let
+  dot_a = Refl `tr2` SDef `tr2` fcong KDef :: D . a == S . (K . a)
+  in fcong2 dot_a `tr2` SDef `tr2` fcong KDef
+
+flip :: forall a b c. F . a . b . c == a . c . b
+flip = let
+  flip_a = Refl `tr2` SDef `tr2` fcong dot `tr2` acong KDef :: F . a == D . (S . a) . K
+  flip_ab = fcong flip_a `tr2` dot :: F . a . b == S . a . (K . b)
+  in fcong flip_ab `tr2` SDef `tr2` acong KDef
+
+type FI = F . I
+
+flip_id :: forall a b. FI . a . b == b . a
+flip_id = Refl `tr2` flip `tr2` fcong id
 
 u :: a
 u = P.undefined
